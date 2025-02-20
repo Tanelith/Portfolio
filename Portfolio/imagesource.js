8b22df6a293ce7a37c4277adb550cb0875d70ed0
Tanelith <tanhiltunen@gmail.com>
2025-02-20 22:37:54 +0200

    First game added

---
+++ Portfolio/imagesource.js
@@ -0,0 +1,22 @@
+const images = [
+    "images/Start.png",
+    "images/enemies.png",
+    "images/gameplay.png"
+];
+
+let currentIndex = 0;
+
+function showImage(index) {
+    const imageElement = document.getElementById("main-image");
+    imageElement.src = images[index];
+}
+
+function previousImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
