252bfc06e3600fe38e3032d2b01f3074897de159
Tanelith <tanhiltunen@gmail.com>
2025-02-22 21:53:13 +0200

    Added Ah, Rats!

--- imagesource.js
+++ imagesource.js
@@ -1,23 +1,40 @@
-const images = [
+const images1 = [
     "images/Start.png",
     "images/enemies.png",
     "images/gameplay.png",
     "images/SnowWarScript.png"
 ];
 
+const images2 = [
+    "images/AhRatsMovement.png",
+    "images/RatFlying.png",
+    "images/Indicator.png"
+];
+
 let currentIndex = 0;
+let currentIndex2 = 0;
 
-function showImage(index) {
-    const imageElement = document.getElementById("main-image");
-    imageElement.src = images[index];
+function showImage(index, imageId, ImageList) {
+    const imageElement = document.getElementById(imageId);
+    imageElement.src = ImageList[index];
 }
 
-function previousImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    showImage(currentIndex);
+function previousImage(imageId, ImageList) {
+    if (imageId == "main-image") {
+        currentIndex = (currentIndex - 1 + ImageList.length) % ImageList.length;
+        showImage(currentIndex, imageId, ImageList);
+    } else if (imageId == "main-image2") {
+        currentIndex2 = (currentIndex2 - 1 + ImageList.length) % ImageList.length;
+        showImage(currentIndex2, imageId, ImageList);
+    }
 }
 
-function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
+function nextImage(imageId, ImageList) {
+    if (imageId == "main-image") {
+        currentIndex = (currentIndex + 1) % ImageList.length;
+        showImage(currentIndex, imageId, ImageList);
+    } else if (imageId == "main-image2") {
+        currentIndex2 = (currentIndex2 + 1) % ImageList.length;
+        showImage(currentIndex2, imageId, ImageList);
+    }
 }
